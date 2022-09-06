696c27b64b42cddf1d1ba7f4e651d7839146bbd0
Lawther <823583958@qq.com>
2022-09-06 11:20:14 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,16 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
-import './styles/fonts/iconfont.css'
-import 'vant/lib/index.css'
-import Vant from 'vant'
-import 'amfe-flexible'
-
 Vue.config.productionTip = false
-Vue.use(Vant)
 
 new Vue({
   router,
