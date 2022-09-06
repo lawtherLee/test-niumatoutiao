382cb4af5ef4e1a525eb72fa868d41a0bd16701f
Lawther <823583958@qq.com>
2022-09-06 16:14:10 +0800

    feat:postcss配置完毕

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,10 +1,10 @@
+// 所有的postcss的插件都是一个函数
+const pxToRem = require("postcss-pxtorem");
 module.exports = {
-  plugins: {
-    'postcss-pxtorem': {
-      rootValue({file}) {
-        return file.indexOf('vant') !== -1 ? 37.5 : 75
-      },
-      propList: ['*']
-    }
-  }
-}
+  plugins: [
+    pxToRem({
+      rootValue: ({ file }) => (/vant/.test(file) ? 37.5 : 75),
+      propList: ["*"], // 所有属性都转成rem
+    }),
+  ],
+};
