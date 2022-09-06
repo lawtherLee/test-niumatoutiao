a2054e7fc14fb69c9159da9b0cd64e27ac577c84
Lawther <823583958@qq.com>
2022-09-06 16:14:10 +0800

    feat:postcss配置完毕

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
-
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <button class="btn">登录</button>
   </div>
 </template>
 
 <script>
+// 1、下载vant
+// 2、main引入组件和css
+// 3、让rem动起来
 export default {}
 </script>
 
 <style scoped>
-
+.btn {
+  width: 500px;
+  height: 88px;
+  background-color: skyblue;
+}
 </style>
